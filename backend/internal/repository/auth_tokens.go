580d937fea387e3fa7754360abbd86c6affa7fb8
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-03-25 16:28:50 -0400

    working on token generation and creation

--- backend/internal/repository/auth_tokens.go
+++ backend/internal/repository/auth_tokens.go
@@ -1,1 +1,30 @@
 package repository
+
+import (
+	"context"
+	"errors"
+
+	"github.com/AlexanderWangY/groupgo-backend/internal/db/sqlc"
+)
+
+var (
+	ErrFailedCreateRefreshToken = errors.New("failed to create refresh token")
+)
+
+type TokenRespository struct {
+	db *sqlc.Queries
+}
+
+func NewTokenRepository(db *sqlc.Queries) *TokenRespository {
+	return &TokenRespository{
+		db: db,
+	}
+}
+
+func (r *TokenRespository) CreateRefreshToken(ctx context.Context, params sqlc.CreateRefreshTokenParams) (*sqlc.AuthRefreshToken, error) {
+	refreshToken, err := r.db.CreateRefreshToken(ctx, params)
+	if err != nil {
+		return nil, ErrFailedCreateRefreshToken
+	}
+	return &refreshToken, nil
+}
